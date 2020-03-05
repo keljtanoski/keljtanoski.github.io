cdc1551f69a6c5d3efed0e1ef5919a17bac867d0
Kostadin Keljtanoki <k3dkdev@gmail.com>
2020-03-05 01:28:12 +0100

    Add new version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+/*
+*=================================
+* Hugo UILite Portfolio v0.8
+*=================================
+*
+* Free version https://uicard.io/products/hugo-uilite
+* Pro version https://uicard.io/products/hugo-uilite-pro
+* Demo https://demo.uicard.io/hugo-uilite-portfolio-demo/
+*
+* Coded By UICardio
+*
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*
+*/
+
+
+let menuBtn = $("#menuBar");
+
+menuBtn.click(function () {
+
+    $('.hamburger-menu').toggleClass('animate');
+
+    if ($(".secondaryMenu").hasClass("active")) {
+
+        $(".secondaryMenu").removeClass("active");
+        setTimeout(function () {
+            $(".primaryMenu").addClass("active");
+        }, 400);
+
+
+    } else {
+        $(".primaryMenu").removeClass("active");
+
+        setTimeout(function () {
+            $(".secondaryMenu").addClass("active");
+        }, 350);
+    }
+});
+
 function scrollTo(target) {
     const top = $(target).offset().top;
     const duration = 500;
